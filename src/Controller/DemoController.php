7df649f83878eaabd753b54a3eabb5da71efca92
Matthias Buczek <matt-buc@orange.fr>
2023-09-12 10:51:46 +0200

    feat:New controller for date

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Locale;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    #[Route('/demo', name: 'app_demo')]
+    public function index(): Response
+    {
+        $date = new \DateTime();
+
+        return $this->render('demo/index.html.twig', [
+            'date' => $date->format('l j F Y'),
+        ]);
+    }
+}
