0cd166bd5ad289f6fe22c20fe1bb7f8dc21e096f
Matthias Buczek <matt-buc@orange.fr>
2023-11-15 18:20:18 +0100

    feat: fixture and .htaccess

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Actor;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $actor = new Actor();
+        $actor->setLastname('DOE');
+        $actor->setFirstname('John');
+        $actor->setDob(new \DateTime('1980-01-01'));
+        $actor->setCreatedAt(new \DateTimeImmutable());
+        $manager->persist($actor);
+        $manager->flush();
+    }
+}
